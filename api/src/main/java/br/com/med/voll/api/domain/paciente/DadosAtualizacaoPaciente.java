741bff7606c476123186723e0ea72663a6e89658
Ayran <ayranvieira29@gmail.com>
2025-09-27 10:18:17 -0300

    Atualizacao para versao final do projeto.

---
+++ api/src/main/java/br/com/med/voll/api/domain/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,12 @@
+package br.com.med.voll.api.domain.paciente;
+
+import jakarta.validation.constraints.NotNull;
+import br.com.med.voll.api.domain.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
+}
